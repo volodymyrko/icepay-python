2eb7a52034f256048d885436cc5dcdc92dcce13a
Volodymyr Kovtun <v.kovtun@levi9.com>
2019-01-11 16:38:08 +0200

    add my changes

--- README.rst
+++ README.rst
@@ -3,12 +3,6 @@ Icepay Python Client
 
 Python client for Icepay REST API, https://icepay.com/docs/rest-api/
 
-Install
--------
-
-::
-
-    pip install icepay-python
 
 Usage
 -----
@@ -17,56 +11,52 @@ Usage
 
     from icepay import IcepayClient
 
-    #init client
-    client = IcepayClient(MERCHANT_ID, SECRET_CODE)
+    BASE_API_URL = 'https://connect.icepay.com/webservice/api/v1/'
+    MERCHANT_ID = 'xxx'
+    SECRET_CODE = 'yyy'
+
+    # init client
+    client = IcepayClient(MERCHANT_ID, SECRET_CODE, BASE_API_URL)
 
 
-    #get own payment methods
-    payment_methods = client.GetMyPaymentMethods()
+    # get own payment methods
+    payment_methods = client.get_my_payment_methods()
 
-    #checkout
-    order_data = client.Checkout({
-        "Amount": "1",
-        "Country": "US",
-        "Currency": "EUR",
-        "Description": "Test",
-        "EndUserIP": "127.0.0.1",
-        "PaymentMethod": "PAYPAL",
-        "Issuer": "DEFAULT",
-        "Language": "EN",
-        "OrderID": "10000031",
-        "URLCompleted": "https://mywebshop.com/Payment/Success",
-        "URLError": "https://mywebshop.com/Payment/Failure"
-    })
+    # start checkout
+    icepay_response = client.checkout(
+        payment_uuid='abc123',  # local payment id
+        amount='10000',  # amount in cents (100 here)
+        country='NL',  # country code
+        user_ip_address='1.2.3.4',
+        description='payment for something',
+        issuer='VISA',  # 'VISA'/'ING'/'DEFAULT' ...
+        payment_method='CREDITCARD',  # 'CREDITCARD'/'IDEAL'/'PAYPAL' ...
+        currency='EUR',
+        language='EN',
+        url_completed='https://site.com/success_url',
+        url_error='https://site.com/error_url',
+    )
 
-    #get payment info by id
-    payment = client.GetPayment(1232)
+    # icepay_response['PaymentScreenURL'] - url where user should be redirected to complete payment
 
-    #validate postback checksum
+    # get payment info by id
+    payment = client.get_payment('1232')
+
+    # validate postback checksum
     # request.POST is dict or QueryDict with key:value map of post data
-    # throws AssertionError on failure
+    # returns True/False
     client.validate_postback(request.POST) 
 
+    # validate web redirect from icepay
+    # get_params is dict with GET params got from icepay in a redirect url
+    payment = client.validate_web_redirect(get_params)
 
-    #generate URL for the BASIC payment mode
-    url = client.getBasicPaymentURL({
-        'IC_OrderID': 123,
-        'IC_Amount': 100,
-        'IC_Currency': 'EUR',
-        'IC_Country': 'US',
-        'IC_URLCompleted': 'https://mywebshop.com/Payment/Success',
-        'IC_URLError': 'https://mywebshop.com/Payment/Failure'
-    })
+    # make refund
+    client.refund(payment_id, amount, currency='EUR')
 
-Advanced usage
---------------
-
-.. code:: python
+    # get all refunds related to some payment
+    client.get_payment_refunds(payment_id):
 
 
-    #make an API call for an endpoint that doesnt have a helper in this lib
-    #note that timestamp is auto added if it's not present
-    values = {
-        #request data
-    }
-    order = client.call_api('POST', 'payment/vaultcheckout', values)
+Used with pthon 3.5.x
+-----
--- icepay/client.py
+++ icepay/client.py
@@ -1,45 +1,30 @@
+import urllib.request
 import json
 import hashlib
 import datetime
-import requests
-import urllib
 
-"""
-Usage:
 
-client = IcepayClient(MERCHANT_ID, SECRET_CODE)
+class IcepayApiClient:
+    CHECKOUT_URL = 'payment/checkout'
+    REFUND_URL = 'refund/requestrefund'
+    PAYMENT_REFUNDS_URL = 'refund/getpaymentrefunds'
+    GET_MY_PAYMENT_METHODS_URL = 'payment/getmypaymentmethods'
+    GET_PAYMENT_URL = 'payment/getpayment'
 
-#create order
-order_data = client.Checkout({
-    "Amount": "1",
-    "Country": "LT",
-    "Currency": "EUR",
-    "Description": "Test",
-    "EndUserIP": "127.0.0.1",
-    "PaymentMethod": "PAYPAL",
-    "Issuer": "DEFAULT",
-    "Language": "EN",
-    "OrderID": "10000031",
-    "URLCompleted": "https://mywebshop.com/Payment/Success",
-    "URLError": "https://mywebshop.com/Payment/Failure"
-})
+    # BASE_API_URL = 'https://connect.icepay.com/webservice/api/v1/'
 
-payment_data = client.GetPayment(order_data['PaymentID'])
-
-"""
-
-class IcepayClient:
-
-    BASE_API_URL = 'https://connect.icepay.com/webservice/api/v1/'
-    BASIC_PAYMENT_URL = 'https://pay.icepay.eu/basic/'
-
-    def __init__(self, merchant_id, secret_code):
-        self.merchant_id = merchant_id
+    def __init__(self, merchant_id, secret_code, base_api_url):
+        self.base_url = base_api_url
         self.secret_code = secret_code
+        self.merchant_id = merchant_id
 
+    def format_timestamp(self, now=None):
+        now = now or datetime.datetime.utcnow()
+        return now.isoformat()[:19]
 
     def calc_checksum(self, url, method, body):
-        assert isinstance(body, str), 'body must be a string'
+        if not isinstance(body, str):
+            raise Exception('body must be a string')
 
         parts = [
             url,
@@ -55,119 +40,136 @@ class IcepayClient:
         m.update(sig.encode('utf8'))
         return m.hexdigest()
 
-    def validate_postback(self, data):
-        """
-        data is QueryDict or dict, values received in POST.
-        calcs and checks sha1 digest
-        """
-        parts = [
-            self.secret_code,
-            self.merchant_id,
-            data.get('Status'),
-            data.get('StatusCode'),
-            data.get('OrderID'),
-            data.get('PaymentID'),
-            data.get('Reference'),
-            data.get('TransactionID'),
-            data.get('Amount'),
-            data.get('Currency'),
-            data.get('Duration'),
-            data.get('ConsumerIPAddress')
-        ]
+    def _prepare_http_headers(self, full_url, method, raw_data):
+        return {
+            'Content-Type': 'application/json',
+            'Checksum': self.calc_checksum(full_url, method, raw_data),
+            'Merchantid': self.merchant_id
+        }
 
-        sig = '|'.join(str(x) for x in parts if x is not None)
-
-        m = hashlib.sha1()
-        m.update(sig.encode('utf8'))
-        digest = m.hexdigest()
-        checksum = data.get('Checksum')
-        assert digest == checksum, 'invalid postback checksum'
-
-    def format_timestamp(self, time=None):
-        #returns timestamp formatted as per icepay specs. must be utc.
-        #returns now() if specific time not provided
-        time = time or datetime.datetime.utcnow()
-        return time.isoformat()[:19]
-
-    def call_api(self, method, endpoint, data={}):
-        assert method in ['POST', 'GET'], 'invalid method: %s, expected POST or GET' % method
+    def _make_api_call(self, method, endpoint, data=None):
+        data = data or {}
 
         if 'Timestamp' not in data:
             data['Timestamp'] = self.format_timestamp()
 
-        full_url = self.BASE_API_URL + endpoint
+        raw_data = json.dumps(data, separators=(',', ':'))
+        full_url = self.base_url + endpoint
+        headers = self._prepare_http_headers(full_url, method, raw_data)
+        utf8_raw_data = raw_data.encode('utf-8')
 
-        data = json.dumps(data, separators=(',', ':'))
+        req = urllib.request.Request(
+            full_url,
+            utf8_raw_data,
+            headers=headers
+        )
 
-        headers = {
-            'MerchantID': str(self.merchant_id),
-            'Checksum': self.calc_checksum(full_url, method, data),
-            'Content-Type': 'application/json'
+        response = urllib.request.urlopen(req)
+        content = response.read().decode('utf-8')
+
+        return json.loads(content)
+
+    def checkout(self, payment_uuid, amount, country, user_ip_address,
+                 description, issuer, payment_method, currency='EUR',
+                 language='EN', url_completed=None, url_error=None):
+        data = {
+            'Timestamp': self.format_timestamp(),
+            'Amount': amount,
+            'Country': country,
+            'Currency': currency,
+            'Description': description,
+            'EndUserIP': user_ip_address,
+            'PaymentMethod': payment_method,
+            'Issuer': issuer,
+            'Language': language,
+            'OrderID': payment_uuid,
         }
 
-        if method == 'POST':
-            r = requests.post(full_url, headers=headers, data=data)
-        else:
-            r = requests.get(full_url, headers=headers, data=data)
+        if url_completed:
+            data['URLCompleted'] = url_completed
 
-        if r.status_code == requests.codes.ok:
-            checksum = self.calc_checksum(full_url, method, str(r.text))
-            assert r.headers['Checksum'] == checksum, 'checksum verification failed'
-            return r.json()
-        else:
-            r.raise_for_status()
+        if url_error:
+            data['URLError'] = url_error
 
+        response = self._make_api_call('POST', self.CHECKOUT_URL, data)
 
-    def GetMyPaymentMethods(self):
-        return self.call_api('POST', 'payment/getmypaymentmethods')
+        return response
 
-    def Checkout(self, values):
-        return self.call_api('POST', 'payment/checkout', values)
+    def get_my_payment_methods(self):
+        return self._make_api_call('POST', self.GET_MY_PAYMENT_METHODS_URL)
 
-    def GetPayment(self, PaymentID):
-        return self.call_api('POST', 'payment/getpayment', {"PaymentID": PaymentID})
+    def validate_postback(self, postback_data):
+        """Calculate and check sha1 digest.
 
-
-    def getBasicPaymentURL(self, values):
+        postback_data is QueryDict or dict, values received in POST.
         """
-        url = client.getBasicPaymentURL({
-            'IC_OrderID': 123,
-            'IC_Amount': 100,
-            'IC_Currency': 'EUR',
-            'IC_Country': 'LT',
-            'IC_URLCompleted': 'http://example.com/success',
-            'IC_URLError': 'http://example.com/fail'
-        })
+        parts = [
+            self.secret_code,
+            self.merchant_id,
+            postback_data['Status'],
+            postback_data['StatusCode'],
+            postback_data['OrderID'],
+            postback_data['PaymentID'],
+            postback_data['Reference'],
+            postback_data['TransactionID'],
+            postback_data['Amount'],
+            postback_data['Currency'],
+            postback_data['Duration'],
+            postback_data['ConsumerIPAddress']
+        ]
+
+        digest = self.list_to_sha1(parts)
+        checksum = postback_data.get('Checksum')
+
+        return digest == checksum
+        # assert digest == checksum, 'invalid postback checksum'
+
+    def validate_web_redirect(self, get_params):
+        """Calc and checks sha1 digest.
+
+        get_params is all GET params that icepay adds to redirect url.
         """
+        parts = [
+            self.secret_code,
+            self.merchant_id,
+            get_params['Status'],
+            get_params['StatusCode'],
+            get_params['OrderID'],
+            get_params['PaymentID'],
+            get_params['Reference'],
+            get_params['TransactionID'],
+        ]
 
-        ALL_FIELDS = ['IC_OrderID', 'IC_Amount', 'IC_Currency', 'IC_PaymentMethod', 'IC_Issuer', 'IC_Country',
-                        'IC_Reference', 'IC_Description', 'IC_URLCompleted', 'IC_URLError', 'IC_PBMID']
-        REQUIRED_FIELDS = ['IC_Amount', 'IC_Currency', 'IC_Country', 'IC_OrderID']
+        digest = self.list_to_sha1(parts)
+        checksum = get_params.get('Checksum')
 
-        CHECKSUM_FIELDS = ['IC_Amount', 'IC_OrderID', 'IC_Reference', 'IC_Currency', 'IC_Country',
-                           'IC_URLCompleted', 'IC_URLError']
+        return digest == checksum
 
+    @staticmethod
+    def list_to_sha1(parts):
+        """Convert input list to sha1."""
+        sig = '|'.join(str(x) for x in parts if x is not None)
 
-        #some validation
-        assert isinstance(values, dict), 'values must be a dict'
-
-        missing_fields = [field for field in REQUIRED_FIELDS if field not in values]
-        assert len(missing_fields) == 0, 'Missing fields: %s' % ', '.join(missing_fields)
-
-        unknown_fields = [key for key in values.keys() if key not in ALL_FIELDS]
-        assert len(unknown_fields) == 0, 'Unknown fields: %s' % ', '.join(unknown_fields)
-
-        assert isinstance(values['IC_Amount'], int), 'IC_Amount must be int, amount in cents.'
-
-        values['IC_MerchantID'] = self.merchant_id
-        values['IC_Version'] = 2
-
-        #calc signature
-        parts = [self.merchant_id, self.secret_code]  + [values.get(field, None) for field in CHECKSUM_FIELDS]
-        sig = '|'.join([str(part if (part or part == 0) else '') for part in parts])
         m = hashlib.sha1()
         m.update(sig.encode('utf8'))
+        return m.hexdigest()
 
-        values['chk'] = m.hexdigest()
+    def get_payment(self, payment_id):
+        data = {
+            'PaymentID': payment_id
+        }
+        return self._make_api_call('POST', self.GET_PAYMENT_URL, data)
 
-        return self.BASIC_PAYMENT_URL + '?' + urllib.parse.urlencode(values)
+    def refund(self, payment_id, amount, currency='EUR'):
+        data = {
+            'PaymentID': payment_id,
+            'RefundAmount': amount,
+            'RefundCurrency': currency,
+        }
+        return self._make_api_call('POST', self.REFUND_URL, data)
+
+    def get_payment_refunds(self, payment_id):
+        data = {
+            'PaymentID': payment_id,
+        }
+        return self._make_api_call('POST', self.PAYMENT_REFUNDS_URL, data)
